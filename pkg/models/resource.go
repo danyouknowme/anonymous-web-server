a2e7cf6d48509196cf0c9348e0669f42cb29f727
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-10-03 18:12:50 +0700

    Added resource model

---
+++ pkg/models/resource.go
@@ -0,0 +1,25 @@
+package models
+
+type Resource struct {
+	IsPublish   bool        `json:"is_publish"`
+	Name        string      `json:"name"`
+	Label       string      `json:"label"`
+	Description string      `json:"description"`
+	Document    string      `json:"document"`
+	Video       string      `json:"video"`
+	Thumbnail   string      `json:"thumbnail"`
+	Images      []string    `json:"images"`
+	Plan        []Plan      `json:"plan"`
+	PatchNotes  []PatchNote `json:"patch_notes"`
+}
+
+type Plan struct {
+	Name  string `json:"name"`
+	Price int64  `json:"price"`
+}
+
+type PatchNote struct {
+	Version  string   `json:"version"`
+	Download string   `json:"download"`
+	Logs     []string `json:"logs"`
+}
