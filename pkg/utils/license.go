5136b7452dcf9665417b2117ad611a9f097dcdec
Thanathip <dannajaeiei@hotmail.com>
2022-10-04 12:03:11 +0700

    Added function generate license and secret code

---
+++ pkg/utils/license.go
@@ -0,0 +1,16 @@
+package utils
+
+func GenerateLicense(username string) string {
+	license := username
+	characters := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	randomString := RandStringBytes(characters, 7)
+	return license + randomString
+}
+
+func GenerateSecretCode() (secretCode []string) {
+	uppercaseCharacters := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	for i := 0; i < 9; i++ {
+		secretCode = append(secretCode, RandStringBytes(uppercaseCharacters, 5))
+	}
+	return secretCode
+}
