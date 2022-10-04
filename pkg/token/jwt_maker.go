2482d0febf85ad353b5110e0df3a4d9de58964e4
Thanathip <dannajaeiei@hotmail.com>
2022-10-04 15:32:12 +0700

    Added verify token and auth middleware function

--- pkg/token/jwt_maker.go
+++ pkg/token/jwt_maker.go
@@ -1,6 +1,7 @@
 package token
 
 import (
+	"errors"
 	"time"
 
 	"github.com/danyouknowme/awayfromus/pkg/util"
@@ -15,4 +16,30 @@ func CreateToken(username string, duration time.Duration) (string, error) {
 
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, payload)
 	return jwtToken.SignedString([]byte(util.AppConfig.SecretKey))
+}
+
+func VerifyToken(token string) (*Payload, error) {
+	keyFunc := func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, ErrInvalidToken
+		}
+		return []byte(util.AppConfig.SecretKey), nil
+	}
+
+	jwtToken, err := jwt.ParseWithClaims(token, &Payload{}, keyFunc)
+	if err != nil {
+		verr, ok := err.(*jwt.ValidationError)
+		if ok && errors.Is(verr.Inner, ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
+	}
+
+	payload, ok := jwtToken.Claims.(*Payload)
+	if !ok {
+		return nil, ErrInvalidToken
+	}
+
+	return payload, nil
 }
