0baca0f02e33afa6d48bbbc73cd15fac0662573b
Thanathip <dannajaeiei@hotmail.com>
2022-10-05 21:56:12 +0700

    Added function update resource expired data

--- pkg/api/user.go
+++ pkg/api/user.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"context"
+	"time"
+
 	"github.com/danyouknowme/awayfromus/pkg/model"
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
@@ -17,4 +20,42 @@ func VerifyUserAdmin(ctx *gin.Context, username string) (bool, error) {
 		return true, nil
 	}
 	return false, nil
+}
+
+func UpdateUserResourceExpiredDate() error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	results, err := userCollection.Find(ctx, bson.M{})
+	if err != nil {
+		return err
+	}
+
+	defer results.Close(ctx)
+	for results.Next(ctx) {
+		var user model.User
+		if err = results.Decode(&user); err != nil {
+			return err
+		}
+
+		resources := []model.UserResource{}
+
+		for _, rs := range user.Resources {
+			resourceUpdated := model.UserResource{}
+			if rs.DayLeft > 0 {
+				resourceUpdated = model.UserResource{
+					Name:    rs.Name,
+					DayLeft: rs.DayLeft - 1,
+					Status:  rs.Status,
+				}
+			}
+			resources = append(resources, resourceUpdated)
+		}
+
+		result := userCollection.FindOneAndUpdate(ctx, bson.M{"username": user.Username}, bson.M{"$set": bson.M{"resources": resources}})
+		if result.Err() != nil {
+			return err
+		}
+	}
+	return nil
 }
