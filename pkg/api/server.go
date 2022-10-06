15cbae301b9563acabdab4b8393717736bfcaa7c
Thanathip <dannajaeiei@hotmail.com>
2022-10-06 16:14:15 +0700

    Added download collection

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -12,6 +12,7 @@ var validate = validator.New()
 var homepageCollection *mongo.Collection = database.GetCollection(database.DB, "homepage")
 var userCollection *mongo.Collection = database.GetCollection(database.DB, "users")
 var resourceCollection *mongo.Collection = database.GetCollection(database.DB, "resources")
+var downloadCollection *mongo.Collection = database.GetCollection(database.DB, "downloads")
 
 func errorResponse(err error) model.ErrorResponse {
 	error := model.ErrorResponse{
