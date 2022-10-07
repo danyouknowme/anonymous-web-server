f44f440fd03da06ad7c65cc4636c2fda77b484e0
Thanathip <dannajaeiei@hotmail.com>
2022-10-07 19:42:13 +0700

    Added helper function for slice

---
+++ pkg/util/slice.go
@@ -0,0 +1,24 @@
+package util
+
+/*
+	Helper function
+	reference: https://stackoverflow.com/questions/34070369/removing-a-string-from-a-slice-in-go
+*/
+func RemoveSecretCode(s []string, r string) []string {
+	for i, v := range s {
+		if v == r {
+			return append(s[:i], s[i+1:]...)
+		}
+	}
+	return s
+}
+
+func Contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
+}
