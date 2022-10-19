4931c91c645e36b09ed3cf8bd5f112b20b28e947
Thanathip <dannajaeiei@hotmail.com>
2022-10-19 15:39:30 +0700

    Seperate config test

--- pkg/util/config_test.go
+++ pkg/util/config_test.go
@@ -12,9 +12,18 @@ import (
 var _ = Describe("Config", func() {
 	util.LoadConfig()
 
-	It("Should load config from environment variables file", func() {
+	It("Should load config port from environment variables file", func() {
+		Expect(util.AppConfig.Port).ShouldNot(BeNil())
 		Expect(util.AppConfig.Port).Should(Equal(os.Getenv("PORT")))
+	})
+
+	It("Should load config mongo uri from environment variables file", func() {
+		Expect(util.AppConfig.MongoUri).ShouldNot(BeNil())
 		Expect(util.AppConfig.MongoUri).Should(Equal(os.Getenv("MONGO_URI")))
+	})
+
+	It("Should load config secret key from environment variables file", func() {
+		Expect(util.AppConfig.SecretKey).ShouldNot(BeNil())
 		Expect(util.AppConfig.SecretKey).Should(Equal(os.Getenv("SECRET_KEY")))
 	})
 })
